dd4682e3e6448d84778100194fd580687c99124a
Akhilaramireddy <akhilaramireddy67@gmail.com>
2023-06-19 20:08:00 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def siraj(rquest):
+    return HttpResponse('<h1><center>Siraj is best blower in rcb team</center></h1>')
+
+
---
+++ app2/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def max(request):
+    return HttpResponse('<h1>mad max.........<h1>')
+
+# Create your views here.
